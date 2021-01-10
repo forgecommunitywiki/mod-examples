f8e5b5c992fd27da6a1a6c5ec86d04fe535bfe98
ChampionAsh5357 <championash5357@gmail.com>
2021-01-10 13:57:12 -0500

    Update all information to Java 8

--- gradlew.bat
+++ gradlew.bat
@@ -17,30 +17,30 @@ set APP_HOME=%DIRNAME%
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
-if defined JAVA_HOME goto findJavaFromJavaHome
+if defined JAVA_8 goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
 
 echo.
-echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo ERROR: JAVA_8 is not set and no 'java' command could be found in your PATH.
 echo.
-echo Please set the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_8 variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
 
 :findJavaFromJavaHome
-set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_8=%JAVA_8:"=%
+set JAVA_EXE=%JAVA_8%/bin/java.exe
 
 if exist "%JAVA_EXE%" goto init
 
 echo.
-echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo ERROR: JAVA_8 is set to an invalid directory: %JAVA_8%
 echo.
-echo Please set the JAVA_HOME variable in your environment to match the
+echo Please set the JAVA_8 variable in your environment to match the
 echo location of your Java installation.
 
 goto fail
